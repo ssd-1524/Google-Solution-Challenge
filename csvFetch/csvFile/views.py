c4874861c1323614a2c2c74793e3929da414bc74
Shreyas S. Deshmukh <ssdeshmukh2003@gmail.com>
2024-01-19 23:41:38 +0530

    Add files via upload

--- csvFetch/csvFile/views.py
+++ csvFetch/csvFile/views.py
@@ -6,23 +6,11 @@ import time
 
 
 def table_view(request):
-    def run_command(command):
-        try:
-            subprocess.run(command, shell=True, check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"Error executing command: {e}")
-
-    def run_command_repeatedly(command, interval_minutes=1):
-        while True:
-            run_command(command)
-            time.sleep(interval_minutes * 60)  # Convert minutes to seconds
-
-    if __name__ == "__main__":
-        # Replace the command below with the command you want to run
-        your_command = "python -c 'from csvFile.resources import import_data_from_csv\nimport_data_from_csv(\"C:/Users/yansh/OneDrive/Desktop/output.csv\")'"
-        run_command_repeatedly(your_command)
-
-    obj = Parking.objects.all()
-    return render(request, 'table.html', {
+    ctr = Parking.objects.filter(Spot_Status=True).count()
+    ctr2 = Parking.objects.filter(Spot_Status=False).count()
+    obj = Parking.objects.all().count()
+    return render(request, 'index.html', {
         'obj_lst': obj,
+        'counter': ctr,
+        'counter2': ctr2,
     })
